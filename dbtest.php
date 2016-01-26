1bb0515221459a11ca2e7563dca903bb93d8150e
VeerMuchandi <veer.muchandi@gmail.com>
2016-01-26 12:53:35 -0500

    small change

--- dbtest.php
+++ dbtest.php
@@ -12,7 +12,7 @@ $connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or d
 
 $query = "SELECT * from users" or die("Error in the consult.." . mysqli_error($connection));
 
-echo "Here is the list of users: <br>";
+echo "Hello All.. Here is the list of users: <br>";
 $rs = $connection->query($query);
 while ($row = mysqli_fetch_assoc($rs)) {
     echo "User Id: ".$row['user_id'] . " User Name: " . $row['username'] . "<br>";
